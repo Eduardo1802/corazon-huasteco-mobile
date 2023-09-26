ad95981cc8414bb5765f8c15c645dc86ebb59bb2
elder <20200710@uthh.edu.mx>
2023-09-26 15:45:32 -0600

    1.0.1 Add structure for the project

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world from another world</Text>
       <StatusBar style="auto" />
+
+      <ActivityIndicator animating={true} color={MD2Colors.purple300} />
     </View>
   );
 }
