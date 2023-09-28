43955679e669ab76ceaa2487dd017b00d1ea0bf6
elder <20200710@uthh.edu.mx>
2023-09-28 15:43:50 -0600

    1.2.2 Add Images

---
+++ src/components/customs/MyCustomComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from 'react-native-paper'
+
+const MyCustomComponent = () => {
+  return (
+    <View>
+        <Text>
+            MyCustomComponent
+        </Text>
+    </View>
+  )
+}
+
+export default MyCustomComponent
