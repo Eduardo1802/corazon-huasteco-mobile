d636e4c8f47cb150796d005bc2ba1538c3f8ca8e
Yael Josafath Flores Alvarado <20200702@uthh.edu.mx>
2023-10-05 19:59:59 -0600

    2.3.2 Upgrade Settings Screen

--- src/screens/Config/Configuracion.js
+++ src/screens/Config/Configuracion.js
@@ -1,19 +1,104 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper'
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Text, Switch as PaperSwitch, List, Divider } from 'react-native-paper';
 
 export const Configuracion = () => {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+
   return (
-    <View style={styles.container}>
-      <Text>Esta es la configuración</Text>
-    </View>
-  )
-}
+    <ScrollView >
+      <View style={styles.container}>
+        <List.Section>
+          <View style={styles.rowContainer}>
+          <List.Item
+              title="Pantalla"
+              titleStyle={{ color: 'black', fontWeight: 'bold' }}
+              left={() => <List.Icon icon="desktop" />}
+            />
+            <Text style={{ flex: 1, textAlign: 'right', color: 'black'}}> Sincronizar dispositivo</Text>
+          </View>
+          <Divider style={styles.dividerStyle}/>
+          <List.Item 
+              title="Preguntas frecuentes" 
+              titleStyle={{ color: 'black', fontWeight: 'bold' }}
+              left={() => <List.Icon icon="folder" />}
+            />
+          <Divider style={styles.dividerStyle}/>
+          <View style={styles.rowContainer}>
+            <List.Item title="Descargar nuevas actualizaciones" 
+              titleStyle={{ color: 'black', fontWeight: 'bold' }} 
+              left={() => <List.Icon icon="folder" />}
+            />
+            <PaperSwitch value={isSwitchOn} onValueChange={onToggleSwitch} />
+          </View>
+          <Divider style={styles.dividerStyle}/>
+          <View style={styles.rowContainer}>
+            <List.Item title="Descargas en segundo plano" 
+              titleStyle={{ color: 'black', fontWeight: 'bold' }} 
+              left={() => <List.Icon icon="folder" />}
+            />  
+            <Text style={{ flex: 1, textAlign: 'right', color: 'black'}}> WiFi</Text>
+          </View>
+          <Divider style={styles.dividerStyle}/>
+          <List.Item 
+              title="Notificaciones" 
+              titleStyle={{ color: 'black', fontWeight: 'bold' }}
+              left={() => <List.Icon icon="folder" />}
+            />
+          <Divider style={styles.dividerStyle}/>
+          <List.Item 
+              title="Acerca de" 
+              titleStyle={{ color: 'black', fontWeight: 'bold' }}
+              left={() => <List.Icon icon="folder" />}
+            />
+          <Divider style={styles.dividerStyle}/>
+          <List.Item 
+              title="Terminos de uso" 
+              titleStyle={{ color: 'black', fontWeight: 'bold' }}
+              left={() => <List.Icon icon="folder" />}
+            />
+          <Divider style={styles.dividerStyle}/>
+          <List.Item 
+              title="Privacidad" 
+              titleStyle={{ color: 'black', fontWeight: 'bold' }}
+              left={() => <List.Icon icon="folder" />}
+            />
+          <Divider style={styles.dividerStyle}/>
+          <List.Item 
+              title="Sobre nosotros" 
+              titleStyle={{ color: 'black', fontWeight: 'bold' }}
+              left={() => <List.Icon icon="folder" />}
+            />
+          <Divider style={styles.dividerStyle}/>
+          <List.Item 
+              title="Contactanos" 
+              titleStyle={{ color: 'black', fontWeight: 'bold' }}
+              left={() => <List.Icon icon="folder" />}
+            />
+          <Divider style={styles.dividerStyle}/>
+        </List.Section>
+      </View>
+    </ScrollView>
+  );
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'left',
+    margin: 10,
+    padding: 20,
+    position: 'relative',
+  },
+  rowContainer: {
+    flexDirection: 'row', 
     alignItems: 'center',
+  },
+  dividerStyle: {
+    backgroundColor: '#b5b5b5',
   },
 });
