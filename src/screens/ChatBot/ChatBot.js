0b819fcafe1059b9dfa644acb410b656c550d20b
Eduardo1802 <20200744@uthh.edu.mx>
2023-10-08 23:54:23 -0600

    2.11.4 Update the chatbot screen

--- src/screens/ChatBot/ChatBot.js
+++ src/screens/ChatBot/ChatBot.js
@@ -1,65 +1,61 @@
 import * as React from "react";
-import { StyleSheet, View, TouchableOpacity } from "react-native";
-import {
-  Text,
-  List,
-  Divider,
-  Chip,
-  Avatar,
-  Card,
-  Searchbar,
-  Drawer,
-} from "react-native-paper";
-
+import { StyleSheet, View, ScrollView } from "react-native";
+import { Text, List, Divider, Avatar, Card, Searchbar} from "react-native-paper";
+ 
 const ChatBot = () => {
   const [searchQuery, setSearchQuery] = React.useState("");
   const onChangeSearch = (query) => setSearchQuery(query);
 
   return (
-    <View style={styles.container}>
-      <Text variant="displaySmall" style={{ marginBottom: 30 }}>
-        Chatbot
-      </Text>
-      <Divider/>
-      <Searchbar
-        placeholder="Envia tu pregunta sobre Huejutla"
-        icon="send"
-        // right={(props) => <Avatar.Icon size={34} icon="send"/>}
-        onChangeText={onChangeSearch}
-        value={searchQuery}
-        style={styles.Searchbar}
-      />
+    <ScrollView>
+      <View style={styles.container}>
+        <Text variant="displaySmall" style={styles.title}>
+          Chatbot
+        </Text>
+        <Divider />
 
-      <Divider />
+        <Searchbar
+          placeholder="Envia tu pregunta sobre Huejutla"
+          icon="send"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+          style={styles.Searchbar}
+        />
+        <Divider />
 
-      <Card.Title
-        // title="Card Title"
-        subtitle="Card Subtitle"
-        style={styles.card}
-        // titleStyle={styles.letters}
-        subtitleStyle={styles.letters}
-        left={(props) => (
-          <Avatar.Image
-            size={44}
-            source={require("../../../assets/img/chatbot/perfil.png")}
-          />
-        )}
-      />
+        <Card style={styles.card}>
+          <Card.Content>
+            <Card.Title
+              title={<Text style={{fontWeight: 'bold'}}>eduazuara0@gmail.com</Text>} 
+              left={(props) => (
+                <Avatar.Image
+                  size={44}
+                  source={require("../../../assets/img/chatbot/perfil.png")}
+                />
+              )}
+            />
+            <Text style={{ textAlign: 'center' }}>¿Cuales es el traje regional de Huejutla?</Text>
+          </Card.Content>
+        </Card>
 
-      <Card.Title
-        // title="Card Title"
-        subtitle="Card Subtitle"
-        style={styles.card}
-        // titleStyle={styles.letters}
-        subtitleStyle={styles.letters}
-        right={(props) => (
-          <Avatar.Image
-            size={44}
-            source={require("../../../assets/img/chatbot/bot.jpg")}
-          />
-        )}
-      />
-    </View>
+        <Card style={styles.card}>
+          <Card.Content>
+            <Card.Title
+              subtitle={<Text style={{ textAlign: 'right', fontWeight: 'bold' }}>ChatBot</Text>}
+              right={(props) => (
+                <Avatar.Image
+                  size={44}
+                  source={require("../../../assets/img/chatbot/bot.jpg")}
+                />
+              )}
+            />
+            <Text style={{ textAlign: 'justify' }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta aspernatur provident dolores debitis, doloribus eum id quisquam molestias accusamus architecto aliquid doloremque ducimus. Nihil vitae odit aliquam adipisci tenetur ipsum.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nisi pariatur ex velit corrupti numquam dolore eius voluptatibus ea obcaecati earum, deserunt beatae labore accusantium, temporibus incidunt ipsam rem. Unde.</Text>
+          </Card.Content>
+        </Card>
+
+      </View>
+    </ScrollView>
   );
 };
 
@@ -71,20 +67,22 @@ const styles = StyleSheet.create({
     padding: 20,
     paddingTop: 50,
   },
-  card: {
-    backgroundColor: "#531949",
-    padding: 10,
-    marginTop: 15,
-    borderRadius: 25,
-  },
-  letters: {
-    color: "white",
+  title: {
+    marginBottom: 30
   },
   Searchbar: {
-    borderWidth: 1,
-    borderColor: "black",
-    backgroundColor: "white",
-    marginBottom: 10,
-    marginTop: 10,
+    marginTop: 20,
+    marginBottom: 20,
+    // borderWidth: 1,
+    // borderColor: "black",
+    // backgroundColor: "white"
   },
+  card: {
+    marginTop: 20,
+    // backgroundColor: "#531949"
+  },
+  letters: {
+    color: "white"
+  }
+
 });
