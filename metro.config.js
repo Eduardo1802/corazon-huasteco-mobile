ca256776b4a28f1bbdb161c97328cfd718d2a6c4
Einar Omar <Kiritoruizeo@gmail.com>
2023-10-24 13:33:17 -0600

    2.33.20 updat add metro.config file

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+const { getDefaultConfig } = require('@react-native/metro-config');
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  return {
+    ...defaultConfig,
+  };
+})();
