e431b6587989762e3f86590121e47f188769cae9
Eduardo1802 <20200744@uthh.edu.mx>
2023-10-24 04:25:25 -0600

    2.32.19 Add comment on the thematics screen

--- src/screens/Thematics/Tematicas.js
+++ src/screens/Thematics/Tematicas.js
@@ -14,6 +14,7 @@ import {
 import { AirbnbRating } from "react-native-ratings";
 import { useAuth } from "../../context/AuthContext";
 import { app } from "../../config/firebase";
+import moment from "moment";
 
 const Tematicas = () => {
   const { user } = useAuth();
@@ -25,20 +26,90 @@ const Tematicas = () => {
   const open = () => setVisible(true);
   // Comentario
   const [comentario, setComentario] = useState("");
-  const [puntuación, setPuntuación] = useState(0);
+  const [puntuación, setPuntuación] = useState(3);
+  // Fecha
+  const fecha = moment().format("YYYY-MM-DD");
+  // Palabras prohibidas
+  const palabrasProhibidas = [
+    "puto",
+    "puta",
+    "pendejo",
+    "pendeja",
+    "imbécil",
+    "estupido",
+    "estupida",
+    "culero",
+    "culera",
+    "zorra",
+    "maldita",
+    "maldito",
+    "pinche",
+    "maricón",
+    "puñetas",
+    "puñetón",
+  ];
 
-  const enviarComentario = () => {
-    console.log("Comentario:", comentario);
-    console.log("Puntuación:", puntuación);
+  // Puntuación
+  const textosPuntuacion = {
+    1: "Inútil",
+    2: "Pobre",
+    3: "De Acuerdo",
+    4: "Bueno",
+    5: "Excelente",
+  };
+
+  const enviarComentario = async () => {
+    if (comentario.trim() === "") {
+      alert("El comentario es requerido.");
+      return;
+    }
+    const comentarioExistente = datosComentarios.find(
+      (comentario) =>
+        comentario.usuario === user.email && comentario.titulo === item.titulo
+    );
+
+    if (comentarioExistente) {
+      alert("Ya has ingresado un comentario en esta temática.");
+      setVisible(false);
+      setComentario("");
+      return;
+    }
+
+    const comentarioSinEspacios = comentario.toLowerCase().replace(/\s/g, "");
+    for (const palabraProhibida of palabrasProhibidas) {
+      if (comentarioSinEspacios.includes(palabraProhibida)) {
+        alert("El comentario contiene palabras prohibidas.");
+        return;
+      }
+    }
+    // console.log("Fecha: ",fecha)
+    // console.log("Comentario:", comentario);
+    // console.log("Puntuación:", puntuación);
+    // console.log("usuario: ", user.email)
+    // console.log("Titulo: ", item.titulo)
+    // console.log("Temática: ", item.tematica)
+    const coleccionRef = app.firestore().collection("comentarios");
+    await coleccionRef.doc(`${new Date().getTime()}`).set({
+      fecha: fecha,
+      puntuacion: String(puntuación),
+      usuario: user.email,
+      titulo: item.titulo,
+      tematica: item.tematica,
+      comentario: comentario,
+    });
+    alert("Comentario enviado con exito.");
+    setVisible(false);
+    setComentario("");
   };
 
   const [datosComentarios, setDatosComentarios] = useState([]);
+
   const obtenerInfo = async () => {
-    const docList = await app.firestore().collection("comentarios").get();
-    const datos = docList.docs
-      .filter((doc) => doc.data().titulo === item.titulo)
-      .map((doc) => doc.data());
-    setDatosComentarios(datos);
+    const coleccionRef = app.firestore().collection("comentarios");
+    coleccionRef.where("titulo", "==", item.titulo).onSnapshot((snapshot) => {
+      const comentarios = snapshot.docs.map((doc) => doc.data());
+      setDatosComentarios(comentarios);
+    });
   };
 
   useEffect(() => {
@@ -65,40 +136,49 @@ const Tematicas = () => {
           </Card.Content>
         </Card>
 
-        {/* FORMULALIO COMENTARIO */}
-        <Portal>
-          <Dialog visible={visible} onDismiss={close}>
-            <Dialog.Title style={styles.title}>Agregar Comentario</Dialog.Title>
-            <Dialog.Content>
-              <Divider style={styles.divider2} />
-              <Text>Usuario: eduazuara0@gmail.com</Text>
-              <Divider style={styles.divider2} />
-              <Text>Foto de perfil</Text>
-              <Avatar.Image
-                size={100}
-                style={styles.img}
-                source={require("../../../assets/img/chatbot/perfil.png")}
-              />
-              <Divider style={styles.divider2} />
-              <TextInput
-                label="Comentario"
-                value={comentario}
-                onChangeText={(text) => setComentario(text)}
-                style={styles.textinput}
-              />
-              <Text>Puntuación</Text>
-              <AirbnbRating
-                size={20}
-                showRating={false}
-                onFinishRating={(value) => setPuntuación(value)}
-              />
-            </Dialog.Content>
-            <Dialog.Actions>
-              <Button onPress={close}>Cancelar</Button>
-              <Button onPress={enviarComentario}>Agregar</Button>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
+        {/* FORMULARIO COMENTARIO */}
+        {user ? (
+          <Portal>
+            <Dialog visible={visible} onDismiss={close}>
+              <Dialog.Title style={styles.title}>
+                <Text>Agregar Comentario</Text>
+              </Dialog.Title>
+              <Dialog.Content>
+                <Divider style={styles.divider2} />
+                <Text>Usuario: {user.email}</Text>
+                <Divider style={styles.divider2} />
+                <Text>Foto de perfil</Text>
+                <Avatar.Image
+                  size={100}
+                  style={styles.img}
+                  source={require("../../../assets/img/chatbot/perfil.png")}
+                />
+                <Divider style={styles.divider2} />
+                <TextInput
+                  label="Comentario"
+                  value={comentario}
+                  onChangeText={(text) => setComentario(text)}
+                  style={styles.textinput}
+                />
+                <Text>Puntuación: {textosPuntuacion[puntuación]}</Text>
+                {/*Usa textosPuntuacion*/}
+                <AirbnbRating
+                  size={20}
+                  showRating={false}
+                  onFinishRating={(value) => setPuntuación(value)}
+                />
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button onPress={close}>Cancelar</Button>
+                <Button onPress={enviarComentario}>Agregar</Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+        ) : (
+          <Portal>
+            <Dialog visible={visible} onDismiss={close}></Dialog>
+          </Portal>
+        )}
 
         {/* COMENTARIOS */}
         <Card style={styles.card}>
@@ -113,6 +193,7 @@ const Tematicas = () => {
                   onPress={open}
                 >
                   Agregar comentario
+                  {/* <Text>"Agregar comentario"</Text> */}
                 </Button>
               </Card.Content>
               <Divider style={{ margin: 10 }} />
@@ -129,7 +210,7 @@ const Tematicas = () => {
                     <AirbnbRating
                       size={20}
                       showRating={false}
-                      defaultRating={datos.puntuacion}
+                      defaultRating={parseInt(datos.puntuacion, 10)}
                     />
                   }
                   left={(props) => (
