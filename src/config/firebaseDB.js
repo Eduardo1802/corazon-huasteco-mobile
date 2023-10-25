47abd82d2484642dbd0ed3e92c5805d56ba0df3e
Einar Omar <Kiritoruizeo@gmail.com>
2023-10-24 21:20:12 -0600

    2.38.21 update firebaseDB.js add persistence/AsyncStorage

--- src/config/firebaseDB.js
+++ src/config/firebaseDB.js
@@ -2,10 +2,11 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getStorage } from "firebase/storage";
 
-import { getReactNativePersistence } from "@firebase/auth";
-import { setPersistence } from "firebase/auth";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCXR191n7jFL7ZUvltSgLnWgd-DlO2n_yo",
   authDomain: "corazon-huasteco-bfbcc.firebaseapp.com",
@@ -15,15 +16,11 @@ const firebaseConfig = {
   appId: "1:636785870470:web:8003e1a5c0f7eb157b3307",
   measurementId: "G-9CWJ2R8N32"
 };
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-setPersistence(auth, getReactNativePersistence(AsyncStorage))
-  .then(() => {
-    console.log("Persistencia configurada correctamente.");
-  })
-  .catch((error) => {
-    console.error("Error al configurar la persistencia:", error);
-  });
 
-export { app, auth, db };// Initialize Firebase
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage= getStorage();
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
