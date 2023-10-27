f7383ab9cbb369c82f3b02d72619ec1072e3b766
JUAN DE DIOS DEL ANGEL ARRIAGA <120742417+20191163@users.noreply.github.com>
2023-10-27 15:30:17 -0600

    2.40.24 update contact us

--- src/screens/Contact us/Contactus.js
+++ src/screens/Contact us/Contactus.js
@@ -1,25 +1,72 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Appbar, Card, TextInput, Button, Text } from 'react-native-paper';
 
-const MiPantalla = () => {
+export default function ContactUsScreen() {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  const handleSendMessage = () => {
+    
+  };
+
   return (
+    <Card>
     <View style={styles.container}>
-      <Text style={styles.text}>¡Hola, esta es mi pantalla en React Native!</Text>
+      <Text variant="displaySmall" style={{ marginBottom: 0, fontWeight: '700' }}>
+        Contactanos
+      </Text>
+      <View style={styles.formContainer}>
+        <TextInput
+          label="Nombre"
+          value={name}
+          onChangeText={text => setName(text)}
+          style={styles.input}
+        />
+        <TextInput
+          label="Correo Electrónico"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          label="Mensaje"
+          value={message}
+          onChangeText={text => setMessage(text)}
+          style={styles.input}
+          multiline
+          numberOfLines={4}
+        />
+        <Button mode="contained" onPress={handleSendMessage} style={styles.button}>
+          Enviar Mensaje
+        </Button>
+        {message && <Text style={styles.confirmationMessage}>Mensaje enviado: {message}</Text>}
+      </View>
     </View>
+    </Card>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    padding: 10,
+    paddingTop: 20,
     alignItems: 'center',
-    backgroundColor: 'white',
+    flex: 1,
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  
+  formContainer: {
+    padding: 16,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 10,
+  },
+  confirmationMessage: {
+    marginTop: 10,
+    color: 'green',
   },
 });
-
-export default MiPantalla;
