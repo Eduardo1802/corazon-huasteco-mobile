e9f4089fa7aa567fd9afbbb53c7ffa6dd2054430
Eduardo1802 <20200744@uthh.edu.mx>
2023-11-25 14:30:46 -0600

    2.46.28 Add functionality to the chatbot

--- src/screens/ChatBot/ChatBot.js
+++ src/screens/ChatBot/ChatBot.js
@@ -1,61 +1,147 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, ScrollView } from "react-native";
-import { Text, List, Divider, Avatar, Card, Searchbar} from "react-native-paper";
- 
-const ChatBot = () => {
-  const [searchQuery, setSearchQuery] = React.useState("");
+import {
+  Text,
+  Button,
+  Divider,
+  Avatar,
+  Card,
+  Searchbar,
+} from "react-native-paper";
+import axios from "axios";
+import { useAuth } from "../../context/AuthContext";
+
+const ChatBot = ({ navigation }) => {
+  const [searchQuery, setSearchQuery] = useState("");
   const onChangeSearch = (query) => setSearchQuery(query);
+  const { user } = useAuth();
+  const [user_query, setUser_query] = useState("");
+  const [predictionData, setPredictionData] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEmptyResponse, setIsEmptyResponse] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const value_query = searchQuery;
+      setUser_query(value_query);
+      setIsLoading(true);
+      setIsEmptyResponse(false);
+      const data = { answer: value_query };
+      const response = await axios.post(
+        "https://eduazuara.pythonanywhere.com/api/predict",
+        data
+      );
+      console.log("Respuesta del servidor:", response.data);
+      const value_response = response.data;
+      setPredictionData(value_response);
+      setIsLoading(false);
+      setSearchQuery("");
+    } catch (error) {
+      console.error("Error al hacer la solicitud POST:", error);
+      setIsLoading(false);
+    }
+  };
 
   return (
-    // <ScrollView>
-      <View style={styles.container}>
-        {/* <Text variant="displaySmall" style={styles.title}>
-          Chatbot
-        </Text> */}
-        <Divider />
+    <>
+      {user ? (
+        <View style={styles.container}>
+          <Divider />
+          <Searchbar
+            placeholder="Pregunta sobre los artículos" // "Envia tu pregunta sobre Huejutla"
+            icon="send"
+            onChangeText={onChangeSearch}
+            onIconPress={handleSubmit}
+            value={searchQuery}
+            style={styles.Searchbar}
+          />
+          <Divider />
 
-        <Searchbar
-          placeholder= "Pregunta sobre los artículos"// "Envia tu pregunta sobre Huejutla"
-          icon="send"
-          onChangeText={onChangeSearch}
-          value={searchQuery}
-          style={styles.Searchbar}
-        />
-        <Divider />
-
-        <Card style={styles.card}>
-          <Card.Content>
-            <Card.Title
-              title={<Text style={{fontWeight: 'bold'}}>eduazuara0@gmail.com</Text>} 
-              left={(props) => (
-                <Avatar.Image
-                  size={44}
-                  source={require("../../../assets/img/chatbot/perfil.png")}
+          {/* PREGUNTA DEL USUARIO */}
+          {user_query !== "" ? (
+            <Card style={styles.card}>
+              <Card.Content>
+                <Card.Title
+                  title={
+                    <Text style={{ fontWeight: "bold" }}>{user.email}</Text>
+                  }
+                  left={(props) => (
+                    <Avatar.Image
+                      size={44}
+                      source={require("../../../assets/img/chatbot/perfil.png")}
+                    />
+                  )}
                 />
-              )}
-            />
-            <Text style={{ textAlign: 'center' }}>¿Cuales es el traje regional de Huejutla?</Text>
-          </Card.Content>
-        </Card>
+                <Text style={{ textAlign: "justify" }}>
+                  Pregunta: {user_query}
+                </Text>
+                <Text style={{ textAlign: "justify" }}>
+                  {isLoading && "Cargando..."}
+                </Text>
+              </Card.Content>
+            </Card>
+          ) : (
+            <Text style={{ textAlign: "justify" }}></Text>
+          )}
 
-        <Card style={styles.card}>
-          <Card.Content>
-            <Card.Title
-              subtitle={<Text style={{ textAlign: 'right', fontWeight: 'bold' }}>ChatBot</Text>}
-              right={(props) => (
-                <Avatar.Image
-                  size={44}
-                  source={require("../../../assets/img/chatbot/bot.jpg")}
+          {/* RESPUESTA DEL CHATBOT */}
+          {predictionData !== "" ? (
+            <Card style={styles.card}>
+              <Card.Content>
+                <Card.Title
+                  subtitle={
+                    <Text style={{ textAlign: "right", fontWeight: "bold" }}>
+                      ChatBot
+                    </Text>
+                  }
+                  right={(props) => (
+                    <Avatar.Image
+                      size={44}
+                      source={require("../../../assets/img/chatbot/bot.jpg")}
+                    />
+                  )}
                 />
-              )}
-            />
-            <Text style={{ textAlign: 'justify' }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta aspernatur provident dolores debitis, doloribus eum id quisquam molestias accusamus architecto aliquid doloremque ducimus. Nihil vitae odit aliquam adipisci tenetur ipsum.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nisi pariatur ex velit corrupti numquam dolore eius voluptatibus ea obcaecati earum, deserunt beatae labore accusantium, temporibus incidunt ipsam rem. Unde.</Text>
-          </Card.Content>
-        </Card>
-
-      </View>
-    // </ScrollView>
+                <Text style={{ textAlign: "justify" }}>
+                  Respuesta: {isLoading && "Cargando..."}
+                </Text>
+                <Text style={{ textAlign: "justify" }}>
+                  {isEmptyResponse
+                    ? "La respuesta está vacía."
+                    : predictionData.answer}
+                </Text>
+              </Card.Content>
+            </Card>
+          ) : (
+            <Text style={{ textAlign: "justify" }}></Text>
+          )}
+        </View>
+      ) : (
+        <View>
+          <Card>
+            <Text
+              style={{
+                textAlign: "center",
+                flexDirection: "row",
+                alignItems: "center",
+                margin: 20,
+              }}
+            >
+              Para poder utilizar el chatbot, es necesario que inicies sesión.
+            </Text>
+            <Card.Content>
+              <Button
+                icon="login"
+                onPress={() => navigation.navigate("Acceso")}
+                style={styles.button2}
+              >
+                Iniciar sesión
+              </Button>
+            </Card.Content>
+          </Card>
+        </View>
+      )}
+    </>
   );
 };
 
@@ -63,26 +149,26 @@ export default ChatBot;
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // padding: 20,
     paddingTop: 20,
   },
   title: {
-    marginBottom: 30
+    marginBottom: 30,
   },
   Searchbar: {
     marginTop: 20,
     marginBottom: 20,
-    // borderWidth: 1,
-    // borderColor: "black",
-    // backgroundColor: "white"
   },
   card: {
     marginTop: 20,
-    // backgroundColor: "#531949"
   },
   letters: {
-    color: "white"
-  }
-
+    color: "white",
+  },
+  button2: {
+    borderWidth: 0.5,
+    borderColor: "#531949",
+    borderRadius: 30,
+    padding: 5,
+    margin: 10,
+  },
 });
