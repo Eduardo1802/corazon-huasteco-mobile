69ab2d849a27050b4dbd347d8fdb3ce06b75fada
JUAN DE DIOS DEL ANGEL ARRIAGA <120742417+20191163@users.noreply.github.com>
2023-11-25 22:53:51 -0600

    2.51.31 Update screen contact

--- src/screens/Contact us/Contactus.js
+++ src/screens/Contact us/Contactus.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Card, Title, Paragraph, Text } from 'react-native-paper';
 
-const MiPantalla = () => {
+const ContactScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>¡Hola, esta es mi pantalla en React Native!</Text>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Title>Contacto</Title>
+          <Paragraph>
+            <Text style={styles.label}>Nombre del Titular:</Text> Eduardo Azuara Redondo.
+          </Paragraph>
+          <Paragraph>
+            <Text style={styles.label}>Domicilio:</Text> Anahuac s/n, Colonia Palma, Huejutla de Reyes, Huejutla de Reyes, CP. 43000, Hidalgo, México
+          </Paragraph>
+          <Paragraph>
+            <Text style={styles.label}>Correo Electrónico:</Text> 20200725@uthh.edu.mx
+          </Paragraph>
+          <Paragraph>
+            <Text style={styles.label}>Teléfono:</Text> 7717292053
+          </Paragraph>
+          <Paragraph>
+            <Text style={styles.label}>Otro Contacto:</Text> 7711189815
+          </Paragraph>
+        </Card.Content>
+      </Card>
     </View>
   );
 };
@@ -13,13 +33,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
+    padding: 16,
   },
-  text: {
-    fontSize: 24,
+  card: {
+    elevation: 4,
+    marginVertical: 8,
+  },
+  label: {
     fontWeight: 'bold',
   },
 });
 
-export default MiPantalla;
+export default ContactScreen;
